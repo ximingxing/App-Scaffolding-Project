407d56465a6c4aa6768652c58e4b3c10eab0d712
Ximing Xing <ximingxing@gmail.com>
2020-01-02 18:39:53 +0800

    update server config

---
+++ blog-app-server/src/main/java/com/ximingxing/blog/BlogServerApplication.java
@@ -0,0 +1,10 @@
+package com.ximingxing.blog;
+
+import org.springframework.boot.SpringApplication;
+
+public class BlogServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogServerApplication.class, args);
+    }
+}
--- polling-app-client/src/index.js
+++ polling-app-client/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app/App';
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter as Router } from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 ReactDOM.render(
     <Router>
-        <App />
-    </Router>, 
+        <App/>
+    </Router>,
     document.getElementById('root')
 );
 
