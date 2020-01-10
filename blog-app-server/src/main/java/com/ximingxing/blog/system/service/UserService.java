23ef162b27a30bdf6238be9aebf60d02262de836
Ximing Xing <ximingxing@gmail.com>
2020-01-10 22:08:50 +0800

    update userservice and project configuration

--- blog-app-server/src/main/java/com/ximingxing/blog/system/service/UserService.java
+++ blog-app-server/src/main/java/com/ximingxing/blog/system/service/UserService.java
@@ -1,56 +1,20 @@
 package com.ximingxing.blog.system.service;
 
 import com.ximingxing.blog.system.entity.User;
-import com.ximingxing.blog.system.enums.UserStatus;
-import com.ximingxing.blog.system.exception.UserNameAlreadyExistException;
-import com.ximingxing.blog.system.repository.UserRepository;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Service;
 
 import java.util.Map;
-import java.util.Optional;
 
 /**
- * @author shuang.kou
+ * Description:
+ * Created By xxm
  */
-@Service
-public class UserService {
+public interface UserService {
+    void saveUser(Map<String, String> registerUser);
 
-    private final UserRepository userRepository;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    User findUserByUserName(String name);
 
-    public UserService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
-        this.userRepository = userRepository;
-        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-    }
+    void deleteUserByUserName(String name);
 
-    public void saveUser(Map<String, String> registerUser) {
-        Optional<User> optionalUser = userRepository.findByUsername(registerUser.get("username"));
-        if (optionalUser.isPresent()) {
-            throw new UserNameAlreadyExistException("User name already exist!Please choose another user name.");
-        }
-        User user = new User();
-        user.setUsername(registerUser.get("username"));
-        user.setPassword(bCryptPasswordEncoder.encode(registerUser.get("password")));
-        user.setRoles("DEV,PM");
-        user.setStatus(UserStatus.CAN_USE);
-        userRepository.save(user);
-    }
-
-    public User findUserByUserName(String name) {
-        return userRepository.findByUsername(name)
-                .orElseThrow(() -> new UsernameNotFoundException("No user found with username " + name));
-    }
-
-    public void deleteUserByUserName(String name) {
-        userRepository.deleteByUsername(name);
-    }
-
-
-    public Page<User> getAllUser(int pageNum, int pageSize) {
-        return userRepository.findAll(PageRequest.of(pageNum, pageSize));
-    }
+    Page<User> getAllUser(int pageNum, int pageSize);
 }
